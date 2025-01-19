4ac55be7453764a30aa7f3bab7fd3c087120b52e
Drills-hub <sdi6566@gmail.com>
2025-01-19 20:43:44 +0900

    fix:swagger & staticfile fix

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -19,3 +19,8 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    password = serializers.CharField(max_length=128, write_only=True)
--- accounts/views.py
+++ accounts/views.py
@@ -3,13 +3,18 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
-from .serializers import UserSerializer
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+from .serializers import UserSerializer,LoginSerializer
 from .models import Role
 
 
 #회원가입
 class SignupAPIView(APIView):
-
+    @swagger_auto_schema(
+            request_body=UserSerializer,
+            responses={201: openapi.Response('Signup successful', UserSerializer)}
+        )
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
@@ -30,20 +35,26 @@ class SignupAPIView(APIView):
 
 #로그인
 class LoginAPIView(APIView):
-
+    @swagger_auto_schema(
+        request_body=LoginSerializer,
+        responses={201: openapi.Response('Login successful', LoginSerializer)}
+    )
     def post(self, request):
-        username = request.data.get("username")
-        password = request.data.get("password")
 
-        user = authenticate(username=username, password=password)
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            username = request.data.get("username")
+            password = request.data.get("password")
 
-        if not user:
+            user = authenticate(username=username, password=password)
+
+            if not user:
+                return Response(
+                    {"message": "아이디 또는 비밀번호를 잘못 입력했습니다."}, status=400
+                )
+            refresh = RefreshToken.for_user(user)
             return Response(
-                {"message": "아이디 또는 비밀번호를 잘못 입력했습니다."}, status=400
+                {
+                    "token": str(refresh.access_token),
+                }
             )
-        refresh = RefreshToken.for_user(user)
-        return Response(
-            {
-                "token": str(refresh.access_token),
-            }
-        )
